00262bab979f191c961286e7c2d49a2126910a47
scutzhe <SCUTEEZHE@163.com>
2016-04-30 23:58:03 +0800

    rewrite android project

--- android/app/src/main/java/com/example/zhe/zhe/MainActivity.java
+++ android/app/src/main/java/com/example/zhe/zhe/MainActivity.java
@@ -1,5 +1,7 @@
-package com.example.administrator.myapplication;
+package com.example.zhe.zhe;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +82,18 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_register){
+             Intent intent = new Intent (this,zhe.zhe.RegisterActivity.class);
+            startActivity(intent);
+        }
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        else if (id == R.id.nav_login) {
+            Intent intent = new Intent(this,zhe.zhe.LoginActivity.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_receive) {
 
         }
 
--- android/app/src/test/java/com/example/zhe/zhe/ExampleUnitTest.java
+++ android/app/src/test/java/com/example/zhe/zhe/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.administrator.myapplication;
+package com.example.zhe.zhe;
 
 import org.junit.Test;
 
